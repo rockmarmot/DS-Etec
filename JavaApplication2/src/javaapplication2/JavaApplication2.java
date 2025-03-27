cbf22ec24b1434055b5232bd7d35357a8792d94d
Lucas <lucasromadev@gmail.com>
2025-03-27 13:47:32 -0300

    Exercicios de mostrar números ímpares e pares

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Numeros impares");
+        for (int i = 1; i <= 100; i += 2) {
+        System.out.println(i);
+        }
+        System.out.println("================================================== ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("================================================== ");
+        
+        
+        System.out.println("Numeros pares");
+                for (int i = 2; i <= 100; i += 2) {
+        System.out.println(i);
+        }
+    }
+    
+}
