ae556d72fe8648c51c2754a884e35f3b77ed8bda
rockmarmot <renatoserafim719@gmail.com>
2025-04-11 14:32:28 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._a_110;
+
+/**
+ *
+ * @author Renato
+ */
+public class App {
+
+    public static void main(String[] args) {
+        int numero = 101;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numero);
+            numero++;
+        }
+
+    }
+}
