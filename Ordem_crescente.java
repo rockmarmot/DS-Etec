060dad41eb4daf757791fe81a1cc4d67a134b5ab
rockmarmot <renatoserafim719@gmail.com>
2025-04-11 14:23:12 -0300

    Add files via upload

---
+++ Ordem_crescente.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.ordem_crescente;
+
+public class Ordem_crescente {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+    
+
