f4cd3d5bb80fb283c86623fb43a74da92a1b07b5
rockmarmot <renatoserafim719@gmail.com>
2025-04-11 14:26:40 -0300

    Add files via upload

---
+++ Ordem_decrescente.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.ordem_decrescente;
+
+
+public class Ordem_decrescente {
+
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
